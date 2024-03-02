a994d4e83b5d889dfadddcb2f3b34e45e610fa1e
= <alfred@stensatter.se>
2024-03-03 00:42:11 +0100

    Added so title is shown in fzf

--- opener/export_b.py
+++ opener/export_b.py
@@ -4,16 +4,20 @@ from html.parser import HTMLParser
 path = "/home/spy/dotfiles2/opener/bookmarks.html"
 
 class MyHTMLParser(HTMLParser):
+    addr = ""
     def handle_starttag(self, tag, attrs):
-        if(tag == "a"):
-            print(attrs[0][1])
+        if(tag == "a" and attrs[0][1] != ""):
+            #print(attrs)
+            self.addr = attrs[0][1]
+            #print(attrs[0][1],end='')
 
     def handle_endtag(self, tag):
         pass
 
     def handle_data(self, data):
-        pass
-        #print("Encountered some data  :", data)
+        if(self.addr != ""):
+            print(data+" | "+self.addr)
+        self.addr = ""
 
 r = open(path,"r")
 parser = MyHTMLParser()
--- opener/opener.sh
+++ opener/opener.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-RES=$(python /home/spy/dotfiles2/opener/export_b.py | fzf)
+RES=$(python /home/spy/dotfiles2/opener/export_b.py | fzf | awk '{split($0,a,"|"); print(a[2])}')
 
 if [ $RES != "" ]; then
    devour firefox $RES
