83f8c18303b4433500a4110986b950fc1949a77d
Alfred Roos <alfred@stensatter.se>
2024-04-04 01:58:13 +0200

    update

--- hypr/.config/hypr/resizeIn.sh
+++ hypr/.config/hypr/resizeIn.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 INC=$1
-OG=`hyprctl getoption general:gaps_in | awk 'FNR==2{print $2}'`
+OG=`hyprctl getoption general:gaps_in | awk 'FNR==1{print $3}'`
 OG=`expr $OG + $INC`
 
 hyprctl keywords general:gaps_in $OG
