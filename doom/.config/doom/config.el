d0f56e3258a857cafc5b4a8bd549742c6178c57c
Alfred Roos <alfred@stensatter.se>
2024-04-18 00:35:12 +0200

    update to emacs

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -157,10 +157,6 @@
      `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
      `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
 
-
-
-
-
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
