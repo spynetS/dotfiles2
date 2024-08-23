f5302539b593232b80f09086793cf708af59f29b
Alfred Roos <alfred@stensatter.se>
2024-08-23 04:02:20 +0200

    update

--- wal/.cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bfc0c0", "#020305" },
-	[SchemeSel] = { "#bfc0c0", "#737985" },
-	[SchemeOut] = { "#bfc0c0", "#C7CDD9" },
-};
--- wal/.cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#020305";
-static const char* selfgcolor   = "#bfc0c0";
-static const char* normbgcolor  = "#7C828E";
-static const char* normfgcolor  = "#bfc0c0";
-static const char* urgbgcolor   = "#737985";
-static const char* urgfgcolor   = "#bfc0c0";
